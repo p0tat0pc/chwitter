0484920ac5cac2a09e05865b928d31a08472811b
p0tat0pc <asherparani@gmail.com>
2024-07-10 14:43:58 +0300

    second commit

---
+++ app.js
@@ -0,0 +1,13 @@
+function App()  {
+    return  (
+        <div className="App">
+            <header className="App -header">
+                <ul>
+                    <li><a href="/home">Home page</a> </li>
+                    <li><a href="/about">About page</a> </li>
+                    <li><a href="/Hello">Hello page</a> </li>
+                </ul>
+            </header>
+        </div>
+    )
+}
